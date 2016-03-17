d0c00e414a90f6b10bd3be390f0d8e342e795fad
Seb Ashton <seb@sebashton.com>
2016-03-17 11:29:49 +0000

    Use _actual_ boolean value as default

--- db/migrate/20160315144823_add_purchasable_to_spree_variant.rb
+++ db/migrate/20160315144823_add_purchasable_to_spree_variant.rb
@@ -1,5 +1,5 @@
 class AddPurchasableToSpreeVariant < ActiveRecord::Migration
   def change
-    add_column :spree_variants, :purchasable, :boolean, default: :true, index: true
+    add_column :spree_variants, :purchasable, :boolean, default: true, index: true
   end
 end
